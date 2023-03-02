916d13a99fd8d7b1496d385420884e3c3ead44fe
Earthmark <earthmark.miller@gmail.com>
2023-03-01 20:56:13 -0800

    Shifted to rkv to be a more reliable data store.

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,71 @@ extern crate rocket;
 mod mdns_record;
 mod store;
 
+use rocket::{
+    http::Status,
+    response::{Redirect, Responder},
+    Request, State,
+};
 use std::path::Path;
-
-use rocket::response::Redirect;
-use rocket::State;
 use store::NameStore;
+use thiserror::Error;
 
-#[derive(Debug)]
-enum Err {
-    Mdns(mdns_record::Error),
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("mDNS: {0}")]
+    Mdns(#[from] mdns_record::Error),
+    #[error("Store: {0}")]
+    Store(#[from] store::Error),
+    #[error("Rocket: {0}")]
+    Rocket(#[from] rocket::Error),
 }
 
-impl From<mdns_record::Error> for Err {
-    fn from(value: mdns_record::Error) -> Self {
-        Err::Mdns(value)
+impl<'r, 'o: 'r> Responder<'r, 'o> for Error {
+    fn respond_to(self, _request: &'r Request<'_>) -> rocket::response::Result<'o> {
+        error!("Internal error: {}", self);
+        Err(Status::InternalServerError)
     }
 }
+
+type Result<T> = std::result::Result<T, Error>;
 
 #[get("/?<token>")]
-async fn index(paths: &State<NameStore>, token: Option<&str>) -> String {
-    if let Some(token) = token {
-        format!("{} => {}", token, paths.get(token).await.unwrap_or("<unbound>".into()))
+fn index(paths: &State<NameStore>, token: Option<&str>) -> Result<String> {
+    let res = if let Some(token) = token {
+        format!(
+            "{} => {}",
+            token,
+            paths.get(token)?.unwrap_or("<unbound>".into())
+        )
     } else {
         format!("Hello world!")
-    }
+    };
+    Ok(res)
 }
 
 #[get("/<token>")]
-async fn resolve_token(paths: &State<NameStore>, token: &str) -> Redirect {
-    if let Some(path) = paths.get(token).await {
+fn resolve_token(paths: &State<NameStore>, token: &str) -> Result<Redirect> {
+    let res = if let Some(path) = paths.get(token)? {
         Redirect::to(path)
     } else {
         Redirect::to(uri!(index(Some(token))))
-    }
+    };
+    Ok(res)
 }
 
 #[post("/<token>", data = "<url>")]
-async fn assign_token(paths: &State<NameStore>, token: &str, url: &str) -> Redirect {
-    paths.set(token, url).await.unwrap();
-    Redirect::to(uri!(resolve_token(token)))
+fn assign_token(paths: &State<NameStore>, token: &str, url: &str) -> Result<Redirect> {
+    paths.set(token, url)?;
+    Ok(Redirect::to(uri!(resolve_token(token))))
 }
 
-#[launch]
-async fn rocket() -> _ {
-    rocket::build()
-        .manage(NameStore::new(
-            Path::new("names.json"),
-            Path::new("names_cache.json"),
-        ).await.unwrap())
+#[rocket::main]
+async fn main() -> Result<()> {
+    let _rocket = rocket::build()
+        .manage(NameStore::new(Path::new("store"))?)
         .mount("/", routes![index, resolve_token, assign_token])
+        .launch()
+        .await?;
+
+    Ok(())
 }
--- src/store.rs
+++ src/store.rs
@@ -1,67 +1,47 @@
-use std::collections::BTreeMap;
-use std::io::ErrorKind;
-use std::path::Path;
-use tokio::fs;
-use tokio::sync::{RwLock, RwLockReadGuard};
+use rkv::{
+    backend::{SafeMode, SafeModeDatabase, SafeModeEnvironment},
+    Manager, Rkv, SingleStore, StoreOptions, Value,
+};
+use std::{path::Path, sync::Arc, sync::RwLock};
 
 pub struct NameStore {
-    path: Box<Path>,
-    backup_path: Box<Path>,
-    records: RwLock<BTreeMap<String, String>>,
+    env: Arc<RwLock<Rkv<SafeModeEnvironment>>>,
+    db: SingleStore<SafeModeDatabase>,
 }
 
+type Result<T> = std::result::Result<T, rkv::StoreError>;
+pub type Error = rkv::StoreError;
+
 impl NameStore {
-    pub async fn new(path: &Path, backup_path: &Path) -> Result<Self, std::io::Error> {
-        let obj = Self {
-            path: path.into(),
-            backup_path: backup_path.into(),
-            records: Default::default(),
-        };
-        obj.read_disk().await?;
-        Ok(obj)
-    }
+    pub fn new(path: &Path) -> Result<Self> {
+        std::fs::create_dir_all(path)?;
+        let mut manager = Manager::<SafeModeEnvironment>::singleton().write()?;
+        let env = manager.get_or_create(path, Rkv::new::<SafeMode>)?;
 
-    pub async fn get(&self, name: &str) -> Option<String> {
-        self.records.read().await.get(name).cloned()
-    }
-    pub async fn set(&self, name: &str, value: &str) -> Result<(), std::io::Error> {
-        let data = self.update_self(name, value).await;
-
-        self.write_disk(&data).await?;
-
-        Ok(())
-    }
-
-    async fn update_self(
-        &self,
-        name: &str,
-        value: &str,
-    ) -> RwLockReadGuard<BTreeMap<String, String>> {
-        let mut store = self.records.write().await;
-        store.entry(name.into()).or_insert(value.into());
-        store.downgrade()
-    }
-
-    async fn write_disk(&self, map: &BTreeMap<String, String>) -> Result<(), std::io::Error> {
-        let data = serde_json::ser::to_vec_pretty(map).unwrap();
-        fs::write(&self.backup_path, data).await?;
-        fs::copy(&self.backup_path, &self.path).await?;
-        Ok(())
-    }
-
-    async fn read_disk(&self) -> Result<(), std::io::Error> {
-        let mut store = self.records.write().await;
-
-        let data = match fs::read(&self.path).await {
-          Ok(data) => data,
-          Err(e) if e.kind() == ErrorKind::NotFound => "{}".as_bytes().to_vec(),
-          _ => fs::read(&self.backup_path).await?
+        let db = {
+            let env_handle = env.read()?;
+            env_handle.open_single("links", StoreOptions::create())?
         };
 
-        let data = serde_json::de::from_slice(&data)?;
+        Ok(Self { db, env })
+    }
 
-        store.clear();
-        *store = data;
+    pub fn get(&self, name: &str) -> Result<Option<String>> {
+        let env = self.env.read()?;
+        let reader = env.read()?;
+        let result = if let Some(Value::Str(s)) = self.db.get(&reader, name)? {
+            Some(s.into())
+        } else {
+            None
+        };
+        Ok(result)
+    }
+
+    pub fn set(&self, name: &str, value: &str) -> Result<()> {
+        let env = self.env.read()?;
+        let mut writer = env.write()?;
+        self.db.put(&mut writer, name, &Value::Str(value))?;
+        writer.commit()?;
         Ok(())
     }
 }
